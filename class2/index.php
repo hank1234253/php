61f22087d5c10d94725e29dbfefbf63799f9f3c3
hank <hank1234253@gmail.com>
2023-04-14 13:37:10 +0800

    if..else

--- class2/index.php
+++ class2/index.php
@@ -1,4 +1,13 @@
 <?php
+    $score=50;
+    if($score>60){
+        echo "成績=".$score;
+        echo "<br>恭喜及格了~";
+    }else{
+        echo "成績=".$score;
+        echo "<br>請再接再厲~";
+    }
+    echo "<hr>";
     for($i=0;$i<=5;$i++){
         for($j=0;$j<$i;$j++){
             echo "*";
@@ -14,12 +23,14 @@
     }
     echo "<hr>";
     
-    for($i=0;$i<=5;$i++){
-        for($j=0;$j<10-2*$i;$j++)
+    for($i=1;$i<=5;$i++){
+        for($j=0;$j<5-$i;$j++)
             echo "&nbsp";
         for($k=0;$k<$i*2-1;$k++){
             echo "*";
         }
         echo "<br>";
     }
+    echo "<hr>";
+
 ?>
