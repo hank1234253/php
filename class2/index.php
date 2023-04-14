6c78b08eaadee1c3797f8d1a0447c73bd79def8c
hank <hank1234253@gmail.com>
2023-04-14 13:49:41 +0800

    switch...case

--- class2/index.php
+++ class2/index.php
@@ -1,4 +1,5 @@
 <?php
+    //if...else
     $score=50;
     if($score>60){
         echo "成績=".$score;
@@ -8,6 +9,27 @@
         echo "<br>請再接再厲~";
     }
     echo "<hr>";
+    //switch...case
+    $level="C";
+    switch ($level) {
+        case 'A':
+            echo "表現優良，請繼續保持";
+            break;
+        case 'B':
+            echo "值得鼓勵，還有進步空間";
+            break;
+        case 'C':
+            echo "需要更多練習";
+            break;
+        case 'D':
+            echo "需要加強基本功";
+            break;
+        default:
+            echo "是否無心學業?";    
+            break;
+    }
+    echo "<hr>";
+    //三角
     for($i=0;$i<=5;$i++){
         for($j=0;$j<$i;$j++){
             echo "*";
@@ -15,6 +37,7 @@
         echo "<br>";
     }
     echo "<hr>";
+    //倒三角
     for($i=5;$i>=0;$i--){
         for($j=0;$j<$i;$j++){
             echo "*";
@@ -22,11 +45,28 @@
         echo "<br>";
     }
     echo "<hr>";
-    
+    //正三角
     for($i=1;$i<=5;$i++){
         for($j=0;$j<5-$i;$j++)
-            echo "&nbsp";
+            echo "a";
         for($k=0;$k<$i*2-1;$k++){
+            echo "*";
+        }
+        echo "<br>";
+    }
+    echo "<hr>";
+    for($i=1;$i<=5;$i++){
+        for($j=0;$j<5-$i;$j++)
+            echo "a";
+        for($k=0;$k<$i*2-1;$k++){
+            echo "*";
+        }
+        echo "<br>";
+    }
+    for($i=1;$i<=4;$i++){
+        for($j=0;$j<$i;$j++)
+            echo "a";
+        for($k=1;$k<10-$i*2;$k++){
             echo "*";
         }
         echo "<br>";
