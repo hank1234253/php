5d64ff31d985c7b4dd97c7f97592d1bedfb92a91
hank <hank1234253@gmail.com>
2023-04-21 09:07:25 +0800

    陣列

---
+++ class3/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a[4]="D";
+        $a['005']="E";
+        $a[]="F";
+        $b[]="B";
+        $c=["A","B",'C'];
+        $d=[5=>'D',2=>"B"];
+        echo $a['005'];
+        echo "<pre>";
+        print_r($a);
+        echo "</pre>";
+
+        echo "<br>";
+        echo $b[0];
+        echo "<br>";
+        echo $c[2];
+        echo "<br>";
+        echo $d[5];
+    ?>
+</body>
+</html>
