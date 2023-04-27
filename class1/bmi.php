6a38490d907cf75752b32c04e1a21bfaca00ead7
hank <hank1234253@gmail.com>
2023-04-27 13:36:02 +0800

    bmi

--- class1/bmi.php
+++ class1/bmi.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $height=$_GET["height"];
-        $weight=$_GET["weight"];
+    if (!empty($_GET["height"]) && !empty($_GET["weight"])) {
+        $height = $_GET["height"];
+        $weight = $_GET["weight"];
         echo "身高：$height 公分<br>";
         echo "體重：$weight 公斤<br>";
-        $height=$height/100;
-        $height=$height*$height;
-        $bmi=$weight/$height;
-        echo "BMI：$bmi"
+        $height = $height / 100;
+        $height = $height * $height;
+        $bmi = round($weight / $height, 2);
+        echo "BMI：$bmi" . "<br>";
+        if ($bmi >= 27) {
+            echo "肥胖";
+        } else if ($bmi >= 24) {
+            echo "體重過重";
+        } else if ($bmi >= 18.5) {
+            echo "健康體重";
+        } else {
+            echo "體重過輕";
+        }
+        echo "<br>";
+    }
+    echo "<a href=\"./index.html\">回上一頁</a>";
     ?>
 </body>
+
 </html>
